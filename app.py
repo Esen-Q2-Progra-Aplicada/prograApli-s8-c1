a2f4f79b58563c5a307fa7307b23e0d3b3d05b98
BalbinoEsen <balbino.aylagas@esen.edu.sv>
2021-06-30 19:24:31 -0600

    added routes for api

--- app.py
+++ app.py
@@ -1,9 +1,11 @@
 from flask import Flask
 from website.site.routes import site
+from website.api.routes import api
 
 app = Flask(__name__)
 
 app.register_blueprint(site)
+app.register_blueprint(api)
 
 if __name__ == "__main__":
     app.run(debug=True)
